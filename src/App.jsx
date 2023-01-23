1d847fbf90c8f74801caa1491d67b6ec65d81031
Jahangir Shirinov <jahangiresh@code.edu.az>
2023-01-23 17:46:53 +0400

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,10 @@ import Blank from "./pages/Blank";
 import Dashboard from "./pages/Dashboard";
 import MainLayout from "./layout/MainLayout";
 import Orders from "./pages/Orders";
+import PageSettings from "./pages/PageSettings";
+import Products from "./pages/Products";
+import Users from "./pages/Users";
+import ProductDetails from "./pages/ProductDetails";
 
 function App() {
   return (
@@ -13,11 +17,12 @@ function App() {
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="orders" element={<Orders />} />
-          <Route path="products" element={<Blank />} />
-          <Route path="customers" element={<Blank />} />
+          <Route path="products" element={<Products />} />
+          <Route path="customers" element={<Users />} />
           <Route path="settings" element={<Blank />} />
           <Route path="stats" element={<Blank />} />
-          <Route path="homepage" element={<Blank />} />
+          <Route path="homepage" element={<PageSettings />} />
+          <Route path="productdetails" element={<ProductDetails />} />
         </Route>
       </Routes>
     </BrowserRouter>
